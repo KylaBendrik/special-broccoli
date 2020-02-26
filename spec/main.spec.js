47679e2072de132f221530439a5e447f9227a965
Beth Jackson <Mbeth.ashton@gmail.com>
2020-02-26 10:18:06 -0500

    check off "dollar side effects?"

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -3,7 +3,9 @@ const { Dollar } = require('../src/main.js')
 
 test('test multiplication', t => {
   let five = new Dollar(5);
-  five.times(2);
-
-  t.is(10, five.amount);
+  let product = five.times(2);
+  t.is(10, product.amount);
+  
+  product = five.times(3);
+  t.is(15, product.amount)
 })
