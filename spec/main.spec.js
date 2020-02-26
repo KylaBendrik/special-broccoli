5e234447bca6fcd3daf95b1b8f1d73ec886ad69a
Beth Jackson <Mbeth.ashton@gmail.com>
2020-02-26 09:55:16 -0500

    create Dollar class

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,6 +1,9 @@
 const test = require('ava');
 const Main = require('../src/main.js')
 
-test('check hello works', t => {
-  t.is(Main.sayHello(), 'Hello!');
-});
+test('test multiplication', t => {
+  let five = new Dollar(5);
+  five.times(2);
+
+  t.is(10, five.amount);
+})
--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+class Dollar {
+  /**
+   * This initializes a new Dollar object
+   * @param {number} amount input any integer (later may be floats?)
+   */
+  constructor(amount) {
+
+  }
+}
+
+
+
+
 module.exports = {
   sayHello(){
     return "Hello!"
